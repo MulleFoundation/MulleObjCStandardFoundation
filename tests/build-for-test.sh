b2fe01a6d5e0905f444225e69e0b9a9b87caabc2
Nat! <nat@mulle-kybernetik.com>
2016-07-23 17:39:06 +0200

    Uniform test shell scropts

--- tests/build-for-test.sh
+++ tests/build-for-test.sh
@@ -1,12 +1,1 @@
-#! /bin/sh
-
-set -e
-
-cd ..
-   mulle-bootstrap build  -c Debug -k "$@"
-
-
-cd build
-   cmake -DCMAKE_OSX_SYSROOT=macosx -DCMAKE_INSTALL_PREFIX="`pwd`/.." -DCMAKE_BUILD_TYPE=Debug ..
-   make -j 4 install
-
+../mulle-tests/build-for-test.sh
